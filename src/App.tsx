e62bc7165506994bcc6b1fcf2a9c7c99be64bab7
eyemovic_tokunaga <tokunaga@eyemovic.com>
2023-01-02 11:06:27 +0900

    2-3:App コンポーネントを分割し、コンポーネントの分割に慣れる

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import styled from 'styled-components'
+
+//styled(_Header)のように書くことで、HTML要素に対応したコンポーネント（styled.div など）以外もスタイリングできます。
+import { Header as _Header } from './Header'
+import { Column } from './Column'
+
+export function App() {
+  return (
+    <Container>
+      <Header />
+
+      <MainArea>
+        <HorizontalScroll>
+          <Column
+            title="TODO"
+            cards={[
+              { id: 'a', text: '朝食をとる🍞' },
+              { id: 'b', text: 'SNSをチェックする🐦' },
+              { id: 'c', text: '布団に入る (:3[___]' },
+            ]}
+          />
+          <Column
+            title="Doing"
+            cards={[
+              { id: 'd', text: '顔を洗う👐' },
+              { id: 'e', text: '歯を磨く🦷' },
+            ]}
+          />
+          <Column title="Waiting" cards={[]} />
+          <Column
+            title="Done"
+            cards={[{ id: 'f', text: '布団から出る (:3っ)っ -=三[＿＿]' }]}
+          />
+        </HorizontalScroll>
+      </MainArea>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+`
+
+const Header = styled(_Header)`
+  flex-shrink: 0;
+`
+
+const MainArea = styled.div`
+  height: 100%;
+  padding: 16px 0;
+  overflow-y: auto;
+`
+
+const HorizontalScroll = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  overflow-x: auto;
+
+  > * {
+    margin-left: 16px;
+    flex-shrink: 0;
+  }
+
+  ::after {
+    display: block;
+    flex: 0 0 16px;
+    content: '';
+  }
+`
