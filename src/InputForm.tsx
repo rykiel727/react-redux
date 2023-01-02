11e532c92fe0a75ceb6002cc870973ccc0efdb71
eyemovic_tokunaga <tokunaga@eyemovic.com>
2023-01-02 12:27:55 +0900

    3-2:InputForm を改良してカスタムフックを理解する

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styled from 'styled-components'
 import * as color from './color'
 import { Button, ConfirmButton } from './Button'
@@ -23,9 +23,13 @@ export function InputForm({
     onConfirm?.()
   }
 
+	const ref = useAutoFitToContentHeight(value)
+
   return (
     <Container className={className}>
+			{/* // ref props として渡すと HTML 要素の実体が保持できる */}
       <Input
+				ref={ref}
         autoFocus
         placeholder="Enter a note"
         value={value}
@@ -65,6 +69,32 @@ export function InputForm({
 //   const { value, onChange } = props
 //   // ...
 // }
+
+/**
+  * テキストエリアの高さを内容に合わせて自動調整する
+  *
+  * @param content テキストエリアの内容
+  */
+function useAutoFitToContentHeight(content: string | undefined) {
+	//ミュータブルな値を保持するオブジェクト (ref) を返します。
+	//State と異なり ref の保持する値が変わってもコンポーネントは再レンダリングされません。
+	const ref = useRef<HTMLTextAreaElement>(null)
+
+	useEffect(
+		() => {
+			const el = ref.current
+			if (!el) return
+
+			const { borderTopWidth, borderBottomWidth } = getComputedStyle(el)
+			el.style.height = 'auto' // 一度 auto にしないと高さが縮まなくなる
+			el.style.height = `calc(${borderTopWidth} + ${el.scrollHeight}px + ${borderBottomWidth})`
+		},
+		// 内容が変わるたびに高さを再計算
+		[content],
+	)
+
+	return ref
+}
 
 const Container = styled.div``
 
